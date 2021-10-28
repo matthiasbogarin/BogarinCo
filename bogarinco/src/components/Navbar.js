8cd25f293787a916265ed4ec120973dbff59c3d5
Matthias Bogarin <mabcsoftwaredev@gmail.com>
2021-10-28 00:03:43 -0400

    added in some new files and starting on the navigation bar compoenent

---
+++ bogarinco/src/components/Navbar.js
@@ -0,0 +1,20 @@
+import React from "react";
+import '../Navbar.css';
+
+
+const Navbar = () => {
+    return (
+        <header className={navbarClasses.join(" ")}>
+
+            <div className="logo">
+
+            </div>
+
+            <div className="navigation">
+                
+            </div>
+        </header>
+    )
+};
+
+export default Navbar;
