ed0e26f232643e8104341486221bf355c576c994
Matthias Bogarin <mabcsoftwaredev@gmail.com>
2021-10-30 15:47:48 -0400

    added in Material UI React Table example

---
+++ bogarinco/src/components/GlobalFilter/index.js
@@ -0,0 +1,1 @@
+export { default as GlobalFilter } from "./GlobalFilter";
--- bogarinco/src/views/Clients/Clients.js
+++ bogarinco/src/views/Clients/Clients.js
@@ -1,10 +1,14 @@
 import React from "react";
+import { Table } from "../../components/Table";
 
 const Clients = props => {
     return (
         <div style={{ textAlign: "center", backgroundColor: "grey", height: "92.43vh", width: "100vw", marginTop: '-20px' }}>
             <h3 style={{ paddingTop: "5%"}}>Clients</h3>
             <p>This is the Clients View</p>
+            <div>
+                <Table />
+            </div>
         </div>
     );
 };
